4f4d245e9556ef61cdd5a6afb59d740914cd4194
rasika <rasikab@yahoo.com>
2024-05-07 22:41:30 +1000

    first commit

---
+++ routes/api-routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// Route to get all notes from db.json
+router.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+// Route to add a new note to db.json
+router.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    newNote.id = generateUniqueId(); // You need to implement this function to generate a unique ID
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes, null, 2), err => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+// Function to generate unique ID (you can use npm packages like uuid)
+function generateUniqueId() {
+  // Implementation of unique ID generation
+}
+
+module.exports = router;
